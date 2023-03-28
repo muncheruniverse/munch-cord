3a160353b0808803e4c6ee0a52066f0e6de812df
Munch <127023971+muncheruniverse@users.noreply.github.com>
2023-03-28 17:18:22 +0100

    Allow for local env (#16)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs')
 const path = require('node:path')
-require('dotenv').config()
+require('dotenv-flow').config()
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
 
 const ManageChannels = require('./db/ManageChannels')
