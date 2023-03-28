9662258ce9336d48700a6318dccee39620aa8129
rk1129 <ryan.kennedy1129@gmail.com>
2023-03-28 16:04:01 +0100

    Allow for local env

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-require('dotenv').config()
+require('dotenv-flow').config()
 
 const sequelize = new Sequelize('database', 'user', 'password', {
   // SQLite only
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 const { REST, Routes } = require('discord.js')
 const fs = require('node:fs')
 const path = require('node:path')
-require('dotenv').config()
+require('dotenv-flow').config()
 
 const commands = []
 // Grab all the command files from the commands directory you created earlier
