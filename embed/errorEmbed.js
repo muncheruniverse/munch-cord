09a00654f046e4cd583f0e7dad9be496fa7e7ee7
Munch <127023971+captain-munch@users.noreply.github.com>
2023-03-29 16:28:18 +0100

    More descriptive. (#21)
    * More descriptive.
    
    * Readme update.
    
    * Flip to secure RPC.
    
    * Update example.

--- embed/errorEmbed.js
+++ embed/errorEmbed.js
@@ -1,7 +1,9 @@
 const { EmbedBuilder } = require('discord.js')
+const axios = require('axios')
 
 const errorEmbed = (error) => {
-  console.log(error)
+  handleError(error)
+
   const embed = new EmbedBuilder()
     .setColor(0xff4000)
     .setTitle('Whoops')
@@ -17,6 +19,44 @@ const errorEmbed = (error) => {
     })
 
   return embed
+}
+
+// Function to send logs to Axiom API
+async function sendLogToAxiom(log) {
+  try {
+    const response = await axios.post(
+      `${process.env.AXIOM_URL}/datasets/${process.env.AXIOM_DATASET}/ingest`,
+      JSON.stringify([log]),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.AXIOM_TOKEN}`,
+        },
+      }
+    )
+
+    if (response.status === 200) {
+      console.log('Log sent to Axiom successfully')
+    } else {
+      console.error('Error sending log to Axiom:', response.statusText)
+    }
+  } catch (error) {
+    console.error('Error sending log to Axiom:', error)
+  }
+}
+
+// Function to handle errors
+function handleError(error) {
+  const log = {
+    timestamp: new Date().toISOString(),
+    message: error.message,
+    stack: error.stack,
+  }
+
+  console.error('Error:', log)
+  if (process.env.AXIOM_TOKEN && process.env.AXIOM_DATASET) {
+    sendLogToAxiom(log)
+  }
 }
 
 module.exports = errorEmbed
