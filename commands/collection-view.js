8d59d16632834ee17884f74dce21dd35bc033527
Munch <blockmunchers@protonmail.com>
2023-03-31 10:59:03 +0100

    Remove extraneous console.log

--- commands/collection-view.js
+++ commands/collection-view.js
@@ -15,39 +15,34 @@ module.exports = {
           channelId: interaction.channelId,
         },
       })
-      if (interaction.user.id === interaction.member.guild.ownerId && channelId) {
-        const collections = await Collections.findAll({
-          where: {
-            channelId: interaction.channelId,
-          },
-          attributes: [
-            'id',
-            'name',
-            'role',
-            [sequelize.fn('COUNT', sequelize.col('Inscriptions.id')), 'inscriptionCount'],
-          ],
-          include: {
-            model: Inscriptions,
-            attributes: [],
-          },
-          group: ['Collections.id'],
+      const collections = await Collections.findAll({
+        where: {
+          channelId: interaction.channelId,
+        },
+        attributes: [
+          'id',
+          'name',
+          'role',
+          [sequelize.fn('COUNT', sequelize.col('Inscriptions.id')), 'inscriptionCount'],
+        ],
+        include: {
+          model: Inscriptions,
+          attributes: [],
+        },
+        group: ['Collections.id'],
+      })
+
+      const embed = infoEmbed('View Collections', 'Collections and their associated role.')
+
+      collections.forEach((collection) => {
+        embed.addFields({
+          name: collection.dataValues.name,
+          value: `${collection.dataValues.role} (${collection.dataValues.inscriptionCount})`,
+          inline: true,
         })
+      })
 
-        const embed = infoEmbed('View Collections', 'Collections and their associated role.')
-
-        collections.forEach((collection) => {
-          embed.addFields({
-            name: collection.dataValues.name,
-            value: `${collection.dataValues.role} (${collection.dataValues.inscriptionCount})`,
-            inline: true,
-          })
-        })
-
-        interaction.reply({ embeds: [embed], ephemeral: true })
-      } else {
-        const embed = errorEmbed(COMMON_ERROR)
-        return interaction.reply({ embeds: [embed], ephemeral: true })
-      }
+      interaction.reply({ embeds: [embed], ephemeral: true })
     } catch (error) {
       const embed = errorEmbed(error)
       return interaction.reply({ embeds: [embed], ephemeral: true })
