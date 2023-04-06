af54116c02c8849ff75fdd28b2dd021a5d490b36
Munch <blockmunchers@protonmail.com>
2023-04-06 16:45:03 +0100

    Smol cleanup and activity.

--- embed/success-embed.js
+++ embed/success-embed.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require('discord.js')
 
 const successEmbed = (title, description) => {
   const embed = new EmbedBuilder().setColor(0x62ff00).setTitle(title).setDescription(description).setFooter({
-    text: 'muncherverse',
+    text: '@muncherverse',
     iconURL: 'https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png',
   })
 
