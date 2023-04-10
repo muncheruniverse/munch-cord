4e5493760dfe46cd9fedddde8d23028886d84d37
Munch <blockmunchers@protonmail.com>
2023-04-10 14:43:02 +0100

    View the number of verifications per collection.

---
+++ commands/collection-verifications.js
@@ -0,0 +1,66 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js')
+const errorEmbed = require('../embed/error-embed')
+const infoEmbed = require('../embed/info-embed')
+const roleEmbed = require('../embed/role-embed')
+const { Collections, Inscriptions } = require('../db/collections-inscriptions')
+const sequelize = require('../db/db-connect')
+const commaNumber = require('comma-number')
+const { Op } = require('sequelize')
+const UserInscriptions = require('../db/user-inscriptions')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('collection-verifications')
+    .setDescription('View all collection verifications for this channel')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+
+  async execute(interaction) {
+    try {
+      const collections = await Collections.findAll({
+        where: {
+          channelId: interaction.channelId,
+        },
+        attributes: [
+          'id',
+          'name',
+          'role',
+          [sequelize.fn('COUNT', sequelize.col('Inscriptions->UserInscriptions.id')), 'userInscriptionCount'],
+        ],
+        include: {
+          model: Inscriptions,
+          attributes: [],
+          include: {
+            model: UserInscriptions,
+            attributes: [],
+          },
+        },
+        having: {
+          userInscriptionCount: {
+            [Op.gt]: 0,
+          },
+        },
+        group: ['Collections.id'],
+      })
+
+      const embed = infoEmbed(
+        'Collection Verifications',
+        'Collections, their associated role and user verification count.'
+      )
+
+      collections.forEach((collection) => {
+        embed.addFields({
+          name: collection.dataValues.name,
+          value: `${roleEmbed(interaction, collection.dataValues.role)} (${commaNumber(
+            collection.dataValues.userInscriptionCount
+          )})`,
+          inline: true,
+        })
+      })
+
+      return interaction.reply({ embeds: [embed], ephemeral: true })
+    } catch (error) {
+      const embed = errorEmbed(error)
+      return interaction.reply({ embeds: [embed], ephemeral: true })
+    }
+  },
+}
