c137bb40075950e36b39ac95a295282f4b71a0fa
rk1129 <ryan.kennedy1129@gmail.com>
2023-04-12 15:59:30 +0100

    Add test script to check if remove role

--- test/re-scan.test.js
+++ test/re-scan.test.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai')
 const sinon = require('sinon')
 const axios = require('axios')
+const { GuildMemberRoleManager } = require('discord.js')
 const sequelize = require('../db/db-connect')
 const CollectionVerifications = require('../commands/collection-verifications')
 const reScan = require('../commands/re-scan')
@@ -12,14 +13,14 @@ describe('re-scan', () => {
 
   it('should successfully rescan inscriptions and call CollectionVerifications.execute', async () => {
     // Mock axios.get
-    const axiosGetStub = sinon.stub(axios, 'get').resolves({ data: { address: 'ownerAddress' } })
+    const axiosGetStub = sinon.stub(axios, 'get').resolves({ data: { address: 'btcAddress2' } })
 
     // Mock sequelize.query
     const sequelizeQueryStub = sinon.stub(sequelize, 'query').resolves([
       [
         {
-          walletAddress: 'walletAddress',
-          userId: 'userId',
+          walletAddress: 'btcAddress1',
+          userId: '123456789012345678',
           inscriptionRef: 'inscriptionRef',
           role: 'role',
         },
@@ -29,13 +30,16 @@ describe('re-scan', () => {
     // Mock CollectionVerifications.execute
     const collectionVerificationsStub = sinon.stub(CollectionVerifications, 'execute').resolves()
 
+    // Mock RolManger
+    const guildMemberRoleManagerStub = sinon.stub(GuildMemberRoleManager.prototype, 'remove').resolves()
+
     // Mock Interaction
     const interaction = {
       channelId: 'channelId',
       member: {
         guild: {
-          roles: { cache: { find: () => ({ id: 'roleId' }) } },
-          members: { cache: { find: () => ({ user: { id: 'userId' }, roles: { remove: () => {} } }) } },
+          roles: { cache: { find: () => ({ id: '123456789012345678' }) } },
+          members: { cache: { find: () => ({ id: '123456789012345678', roles: GuildMemberRoleManager.prototype, }) } },
         },
       },
       reply: sinon.stub().resolves(),
@@ -46,6 +50,7 @@ describe('re-scan', () => {
     expect(axiosGetStub.calledOnce).to.be.true
     expect(sequelizeQueryStub.calledOnce).to.be.true
     expect(collectionVerificationsStub.calledOnce).to.be.true
+    expect(guildMemberRoleManagerStub.calledOnce).to.be.true
     expect(interaction.reply.calledOnce).to.be.false
   })
 })
