9000f0830a25268232a232c0306fcc3f332b55a5
Munch <blockmunchers@protonmail.com>
2023-04-13 09:14:52 +0100

    Adds a test to ensure only old address links have roles removed.

--- test/re-scan.test.js
+++ test/re-scan.test.js
@@ -39,7 +39,7 @@ describe('re-scan', () => {
       member: {
         guild: {
           roles: { cache: { find: () => ({ id: '123456789012345678' }) } },
-          members: { cache: { find: () => ({ id: '123456789012345678', roles: GuildMemberRoleManager.prototype, }) } },
+          members: { cache: { find: () => ({ id: '123456789012345678', roles: GuildMemberRoleManager.prototype }) } },
         },
       },
       reply: sinon.stub().resolves(),
@@ -52,5 +52,51 @@ describe('re-scan', () => {
     expect(collectionVerificationsStub.calledOnce).to.be.true
     expect(guildMemberRoleManagerStub.calledOnce).to.be.true
     expect(interaction.reply.calledOnce).to.be.false
+  })
+
+  it('should unset the btcAddress1 role accordingly as inscription location changed', async () => {
+    // Mock axios.get
+    const axiosGetStub = sinon.stub(axios, 'get').resolves({ data: { address: 'btcAddress2' } })
+
+    // Mock sequelize.query
+    const sequelizeQueryStub = sinon.stub(sequelize, 'query').resolves([
+      {
+        walletAddress: 'btcAddress1',
+        userId: '123456789012345678',
+        inscriptionRef: 'inscriptionRef',
+        role: 'role1',
+      },
+      {
+        walletAddress: 'btcAddress2',
+        userId: '123456789012345678',
+        inscriptionRef: 'inscriptionRef',
+        role: 'role2',
+      },
+    ])
+
+    // Mock CollectionVerifications.execute
+    const collectionVerificationsStub = sinon.stub(CollectionVerifications, 'execute').resolves()
+
+    // Mock RolManger
+    const removeRoleStub = sinon.stub(GuildMemberRoleManager.prototype, 'remove').resolves()
+
+    // Mock Interaction
+    const interaction = {
+      channelId: 'channelId',
+      member: {
+        guild: {
+          roles: { cache: { find: () => ({ id: '123456789012345678' }) } },
+          members: { cache: { find: () => ({ id: '123456789012345678', roles: GuildMemberRoleManager.prototype }) } },
+        },
+      },
+      reply: sinon.stub().resolves(),
+    }
+
+    await reScan.execute(interaction)
+
+    expect(axiosGetStub.calledTwice).to.be.true
+    expect(sequelizeQueryStub.calledOnce).to.be.true
+    expect(collectionVerificationsStub.calledOnce).to.be.true
+    expect(removeRoleStub.calledOnce).to.be.true
   })
 })
