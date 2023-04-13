2ab28ebc77dba97319cbb94b24c1b0150d3dda11
Munch <blockmunchers@protonmail.com>
2023-04-13 09:33:42 +0100

    Catches the error.

--- test/re-scan.test.js
+++ test/re-scan.test.js
@@ -99,4 +99,40 @@ describe('re-scan', () => {
     expect(collectionVerificationsStub.calledOnce).to.be.true
     expect(removeRoleStub.calledOnce).to.be.true
   })
+
+  it('should return a warning embed when a SequelizeUniqueConstraintError is thrown', async () => {
+    // Mock axios.get
+    const axiosGetStub = sinon.stub(axios, 'get').resolves({ data: { address: 'btcAddress2' } })
+
+    // Create custom error object
+    const sequelizeUniqueConstraintError = new Error('SequelizeUniqueConstraintError')
+    sequelizeUniqueConstraintError.name = 'SequelizeUniqueConstraintError'
+
+    // Mock sequelize.query
+    const sequelizeQueryStub = sinon.stub(sequelize, 'query').rejects(sequelizeUniqueConstraintError)
+
+    // Mock Interaction
+    const interaction = {
+      channelId: 'channelId',
+      member: {
+        guild: {
+          roles: { cache: { find: () => ({ id: '123456789012345678' }) } },
+          members: { cache: { find: () => ({ id: '123456789012345678', roles: GuildMemberRoleManager.prototype }) } },
+        },
+      },
+      reply: sinon.stub().resolves(),
+    }
+
+    await reScan.execute(interaction)
+
+    // Wont be called as the error is caught
+    expect(axiosGetStub.called).to.be.false
+    expect(sequelizeQueryStub.calledOnce).to.be.true
+    expect(interaction.reply.calledOnce).to.be.true
+    expect(interaction.reply.firstCall.args[0].embeds[0].data.title).to.equal('Add verify bot')
+    expect(interaction.reply.firstCall.args[0].embeds[0].data.description).to.equal(
+      'The bot is already in the channel.'
+    )
+    expect(interaction.reply.firstCall.args[0].ephemeral).to.be.true
+  })
 })
