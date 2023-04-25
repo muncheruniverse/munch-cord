a7965a6b4600468141c52a8b48b8eae4af4a7cbe
Munch <127023971+captain-munch@users.noreply.github.com>
2023-04-25 17:20:06 +0100

    Rk#67/channel add check (#68)
    * Check if the bot has permissions for channel-add command
    
    * Update text.
    
    * Add test script for permission checking
    
    * Remove console.log
    
    ---------
    
    Co-authored-by: rk1129 <ryan.kennedy1129@gmail.com>

--- commands/channel-add.js
+++ commands/channel-add.js
@@ -12,6 +12,27 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
   async execute(interaction) {
     try {
+      const channel = interaction.channel
+      const botMember = interaction.channel.guild.members.cache.get(interaction.client.user.id)
+      const hasPermissions = botMember
+        .permissionsIn(channel)
+        .has([
+          PermissionFlagsBits.ViewChannel,
+          PermissionFlagsBits.SendMessages,
+          PermissionFlagsBits.ManageRoles,
+          PermissionFlagsBits.UseApplicationCommands,
+        ])
+      if (hasPermissions === false) {
+        const warning = warningEmbed(
+          'Missing Permissions',
+          "The bot doesn't have the relevant permissions to respond, if this is a private channel you must specifically add the bot's role to the channel."
+        )
+        return interaction.reply({
+          embeds: [warning],
+          ephemeral: true,
+        })
+      }
+
       await ManageChannels.create({
         channelId: interaction.channelId,
       })
