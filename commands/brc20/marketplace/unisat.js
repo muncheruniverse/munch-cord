1f2cbbc356024dbd004f63b4023223f6b0710764
rk1129 <ryan.kennedy1129@gmail.com>
2023-05-24 14:54:27 +0100

    Add bestinslot api service for brc20

--- commands/brc20/marketplace/unisat.js
+++ commands/brc20/marketplace/unisat.js
@@ -6,9 +6,15 @@ const header = {
 
 const isValidBrc20 = async (symbol) => {
   try {
-    const url = `https://www.oklink.com/api/v5/explorer/btc/token-details?token=${symbol}`
-    const { data } = await axios.get(url, { headers: header })
-    if (data.data.length > 0) return true
+    if (process.env.BRC20_API_PROVIDER === 'oklink') {
+      const url = `https://www.oklink.com/api/v5/explorer/btc/token-details?token=${symbol}`
+      const { data } = await axios.get(url, { headers: header })
+      if (data.data.length > 0) return true
+    } else if (process.env.BRC20_API_PROVIDER === 'bestinslot') {
+      const url = `https://brc20api.bestinslot.xyz/v1/get_brc20_ticker/${symbol}`
+      const { data } = await axios.get(url)
+      if (data.ticker.length > 0) return true
+    }
     return false
   } catch (error) {
     return false
--- utils/verify-brc20.js
+++ utils/verify-brc20.js
@@ -18,15 +18,22 @@ const getTotalBrc20Numbers = async (address) => {
 
 const getOwnedSymbols = async (address) => {
   const ownedSymbols = []
-  const totalPageNumber = await getTotalBrc20Numbers(address)
-  if (totalPageNumber === 0) return ownedSymbols
+  if (process.env.BRC20_API_PROVIDER === 'oklink') {
+    const totalPageNumber = await getTotalBrc20Numbers(address)
+    if (totalPageNumber === 0) return ownedSymbols
 
-  for (let i = 1; i < totalPageNumber + 1; i++) {
-    const url = `https://www.oklink.com/api/v5/explorer/btc/address-balance-list?address=${address}&limit=${PAGINATED_AMOUNT}&page=${i}`
-    const res = await axios.get(url, { headers: header })
-    for (const brc20sData of res.data.data[0].balanceList) {
-      ownedSymbols.push(brc20sData.token)
+    for (let i = 1; i < totalPageNumber + 1; i++) {
+      const url = `https://www.oklink.com/api/v5/explorer/btc/address-balance-list?address=${address}&limit=${PAGINATED_AMOUNT}&page=${i}`
+      const res = await axios.get(url, { headers: header })
+      for (const brc20sData of res.data.data[0].balanceList) {
+        ownedSymbols.push(brc20sData.token)
+      }
     }
+  } else if (process.env.BRC20_API_PROVIDER === 'bestinslot') {
+    const url = `https://brc20api.bestinslot.xyz/v1/get_brc20_balance/${address}`
+    const res = await axios.get(url)
+    const symbols = res.data.map((item) => item.tick)
+    ownedSymbols.push(...symbols)
   }
 
   return ownedSymbols
