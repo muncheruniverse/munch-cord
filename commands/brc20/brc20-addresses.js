4881bc7d3e52c773dd7504f40c2f997fa12aa246
Munch <127023971+captain-munch@users.noreply.github.com>
2023-06-14 10:52:23 +0100

    Rk/add collection addresses command (#82)
    * Add collection-addresses command
    
    * Add brc20-addresses command
    
    ---------
    
    Co-authored-by: rk1129 <ryan.kennedy1129@gmail.com>

---
+++ commands/brc20/brc20-addresses.js
@@ -0,0 +1,75 @@
+const { SlashCommandBuilder, PermissionFlagsBits, AttachmentBuilder } = require('discord.js')
+const errorEmbed = require('../../embed/error-embed')
+const infoEmbed = require('../../embed/info-embed')
+const Brc20s = require('../../db/brc20s')
+const UserBrc20s = require('../../db/user-brc20s')
+const { UserAddresses } = require('../../db/user-addresses')
+const { getBrc20Balance } = require('../../utils/verify-brc20')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('brc20-addresses')
+    .setDescription('View all holders addresses and amount')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+    .addStringOption((option) => option.setName('name').setDescription('The brc20 name to search').setRequired(true)),
+
+  async execute(interaction) {
+    try {
+      const brc20s = await Brc20s.findAll({
+        where: {
+          channelId: interaction.channelId,
+          name: interaction.options.getString('name'),
+        },
+        attributes: ['id', 'name'],
+        include: {
+          model: UserBrc20s,
+          include: {
+            model: UserAddresses,
+            attributes: ['userId', 'walletAddress'],
+          },
+        },
+      })
+
+      const data = []
+
+      await interaction.deferReply({
+        ephemeral: true,
+      })
+
+      for (const brc20 of brc20s) {
+        for (const userBrc20 of brc20.UserBrc20s) {
+          const address = userBrc20.UserAddress.walletAddress
+          const balance = await getBrc20Balance(address, brc20.name)
+          data.push({
+            userId: userBrc20.UserAddress.userId,
+            walletAddress: address,
+            balance,
+          })
+        }
+      }
+
+      const json = new AttachmentBuilder(Buffer.from(JSON.stringify(data, 0, 2)), { name: 'brc20.json' })
+
+      // Create a csv from the result in the format of userId, walletAddress, balance
+      const csv = new AttachmentBuilder(
+        Buffer.from(
+          data
+            .map((row) => `${row.userId},${row.walletAddress},${row.balance}`)
+            .join('\n')
+            .concat('\n'),
+          'utf-8'
+        ),
+        { name: 'brc20.csv' }
+      )
+
+      const embed = infoEmbed(
+        'View Brc20 Addresses',
+        `Verified owners of the the ${interaction.options.getString('name')} brc20 are attached above.`
+      )
+      return interaction.editReply({ embeds: [embed], ephemeral: true, files: [json, csv] })
+    } catch (error) {
+      const embed = errorEmbed(error)
+      return interaction.reply({ embeds: [embed], ephemeral: true })
+    }
+  },
+}
---
+++ commands/collection/collection-addresses.js
@@ -0,0 +1,60 @@
+const { SlashCommandBuilder, PermissionFlagsBits, AttachmentBuilder } = require('discord.js')
+const errorEmbed = require('../../embed/error-embed')
+const infoEmbed = require('../../embed/info-embed')
+const sequelize = require('../../db/db-connect')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('collection-addresses')
+    .setDescription('View all inscriptions and addresses')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+    .addStringOption((option) =>
+      option.setName('name').setDescription('The collection name to search').setRequired(true)
+    ),
+
+  async execute(interaction) {
+    try {
+      const query = `
+        SELECT ua.userId, ua.walletAddress, i.inscriptionRef
+        FROM Collections c
+        JOIN Inscriptions i ON c.id = i.collectionId
+        JOIN UserInscriptions ui ON i.id = ui.inscriptionId
+        JOIN UserAddresses ua ON ui.userAddressId = ua.id
+        WHERE c.channelId = ${interaction.channelId}
+        AND c.name = '${interaction.options.getString('name')}'
+        AND c.deletedAt IS NULL
+        AND ui.deletedAt IS NULL;
+      `
+
+      const result = await sequelize.query(query, { type: sequelize.QueryTypes.SELECT })
+
+      await interaction.deferReply({
+        ephemeral: true,
+      })
+
+      const json = new AttachmentBuilder(Buffer.from(JSON.stringify(result, 0, 2)), { name: 'collection.json' })
+
+      // Create a csv from the result in the format of userId, walletAddress, inscriptionRef
+      const csv = new AttachmentBuilder(
+        Buffer.from(
+          result
+            .map((row) => `${row.userId},${row.walletAddress},${row.inscriptionRef}`)
+            .join('\n')
+            .concat('\n'),
+          'utf-8'
+        ),
+        { name: 'collection.csv' }
+      )
+
+      const embed = infoEmbed(
+        'View Collection Addresses',
+        `Verified owners of the the ${interaction.options.getString('name')} collection are attached above.`
+      )
+
+      return interaction.editReply({ embeds: [embed], ephemeral: true, files: [json, csv] })
+    } catch (error) {
+      const embed = errorEmbed(error)
+      return interaction.reply({ embeds: [embed], ephemeral: true })
+    }
+  },
+}
